24ddd9aee82c88904729c06cc94c74ebdc0d640d
N-halila <al1609106@upy.edu.mx>
2019-07-02 13:34:08 -0500

    Distance sensor

---
+++ controllers/crazybot_Quintal_Nora/build/release/crazybot_Quintal_Nora.d
@@ -0,0 +1,5 @@
+build/release/crazybot_Quintal_Nora.o: crazybot_Quintal_Nora.c \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h \
+ /usr/local/webots/include/controller/c/webots/motor.h
