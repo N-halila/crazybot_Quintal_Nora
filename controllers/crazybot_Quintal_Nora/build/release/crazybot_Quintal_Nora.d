b2f4ad3eead0b17fb65391ab1060a3cbfbe8b9e0
N-halila <al1609106@upy.edu.mx>
2019-07-03 12:00:08 -0500

    controller done and movements with keyboard

--- controllers/crazybot_Quintal_Nora/build/release/crazybot_Quintal_Nora.d
+++ controllers/crazybot_Quintal_Nora/build/release/crazybot_Quintal_Nora.d
@@ -2,4 +2,7 @@ build/release/crazybot_Quintal_Nora.o: crazybot_Quintal_Nora.c \
  /usr/local/webots/include/controller/c/webots/robot.h \
  /usr/local/webots/include/controller/c/webots/types.h \
  /usr/local/webots/include/controller/c/webots/nodes.h \
- /usr/local/webots/include/controller/c/webots/motor.h
+ /usr/local/webots/include/controller/c/webots/motor.h \
+ /usr/local/webots/include/controller/c/webots/keyboard.h \
+ /usr/local/webots/include/controller/c/webots/distance_sensor.h \
+ /usr/local/webots/include/controller/c/webots/position_sensor.h
--- controllers/crazybot_Quintal_Nora/crazybot_Quintal_Nora.c
+++ controllers/crazybot_Quintal_Nora/crazybot_Quintal_Nora.c
@@ -12,11 +12,16 @@
  */
 #include <webots/robot.h>
 #include <webots/motor.h>
+#include <webots/keyboard.h>
+#include <webots/distance_sensor.h>
+#include <webots/position_sensor.h>
 
+#include <stdio.h>
 /*
  * You may want to add macros here.
  */
 #define TIME_STEP 64
+#define pi 3.1416
 
 /*
  * This is the main program.
@@ -25,24 +30,49 @@
  */
 int main(int argc, char **argv)
 {
+  int  key_letter;
   /* necessary to initialize webots stuff */
   wb_robot_init();
-
-  /*
+  wb_keyboard_enable(TIME_STEP);
+  double lin_vel;
+  double dis_sen;
+  double ps_der;
+  double ps_izq;
+  double vel;
+  double rpm;
+  double radio=0.075;  /*
    * You should declare here WbDeviceTag variables for storing
    * robot devices like this:
    *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
    *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
    */
+   //llantas
   WbDeviceTag wheel_right = wb_robot_get_device("motor_right");
   WbDeviceTag wheel_left = wb_robot_get_device("motor_left");
+ 
+  WbDeviceTag ds = wb_robot_get_device("ds");
+  wb_distance_sensor_enable(ds,TIME_STEP);  
+  
+  //this is the position sensor device, darle nombre a la variable
+  WbDeviceTag ps_r = wb_robot_get_device("ps_right");
+  WbDeviceTag ps_l = wb_robot_get_device("ps_left");
+  //habilitar el sensor
+  wb_position_sensor_enable(ps_r, TIME_STEP);
+  wb_position_sensor_enable(ps_l, TIME_STEP);
+  
+  
+  
+  
+   wb_motor_set_position(wheel_right, INFINITY);   
+   wb_motor_set_position(wheel_left, INFINITY);
   /* main loop
    * Perform simulation steps of TIME_STEP milliseconds
    * and leave the loop when the simulation is over
    */
   while (wb_robot_step(TIME_STEP) != -1) {
-
-    /*
+      key_letter = wb_keyboard_get_key(); 
+      
+      /*
      * Read the sensors :
      * Enter here functions to read sensor data, like:
      *  double val = wb_distance_sensor_get_value(my_sensor);
@@ -54,11 +84,74 @@ int main(int argc, char **argv)
      * Enter here functions to send actuator commands, like:
      * wb_differential_wheels_set_speed(100.0,100.0);
      */
+     
+     if (key_letter== WB_KEYBOARD_RIGHT){
+     vel = 26.6666666666666668;
+     wb_motor_set_velocity(wheel_right, -vel);   
+     wb_motor_set_velocity(wheel_left, vel);
+     rpm= (vel* (2*pi))/60;
+     lin_vel= (((2*pi)*radio)/60)*rpm;
+     printf("the linear velocity is %lf\n", lin_vel);
+     printf("the revolution per minute are %lf\n", rpm);
+     }
+     
+     else if (key_letter== WB_KEYBOARD_LEFT){
+     vel = 26.66666666666666668;
+     wb_motor_set_velocity(wheel_right, 26.666666666666668);   
+     wb_motor_set_velocity(wheel_left, -26.666666666666668);
+     rpm= (vel* (2*pi))/60;
+     lin_vel= (((2*pi)*radio)/60)*rpm;
+     printf("the linear velocity is %lf\n", lin_vel);
+     printf("the revolution per minute are %lf\n", rpm);
+     }
+     
+     else if (key_letter== WB_KEYBOARD_UP){
+     vel = 40;
+     wb_motor_set_velocity(wheel_right, vel);   
+     wb_motor_set_velocity(wheel_left, vel);    
+     rpm= (vel* (2*pi))/60;
+     lin_vel= (((2*pi)*radio)/60)*rpm;
+     printf("the linear velocity is %lf\n", lin_vel);
+     printf("the revolution per minute are %lf\n", rpm);
+    
+     
+     
+     }
+     
+      else if (key_letter== WB_KEYBOARD_DOWN){
+      vel= 1.3333333333333335;
+     wb_motor_set_velocity(wheel_right, -vel);   
+     wb_motor_set_velocity(wheel_left, -vel);
+     
+     rpm= (vel* (2*pi))/60;
+     lin_vel= (((2*pi)*radio)/60)*rpm;
+     printf("the linear velocity is %lf\n", lin_vel);
+     printf("the revolution per minute are %lf\n", rpm);
+     }
+     
+     else {
+     wb_motor_set_velocity(wheel_right, 0);   
+     wb_motor_set_velocity(wheel_left, 0);
+   
+   
+     }
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
   };
 
   /* Enter your cleanup code here */
 
   /* This is necessary to cleanup webots resources */
+  
+  
   wb_robot_cleanup();
 
   return 0;
